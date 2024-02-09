20d5cf70430db5e9dc4c8f705cebcbe72c1489f9
NimbleNitesh <nitesh620137@gmail.com>
2024-02-09 23:36:32 +0530

    corrected the value stored as jsonKey

--- src/components/Radio/index.js
+++ src/components/Radio/index.js
@@ -1,19 +1,20 @@
 import React from 'react'
 
 const index = ({ label, options, description, handleChange, name }) => {
-  const options_arr = options.map(option => option.label);
+  // const options_arr = options.map(option => option.label);
+
   return (
     <div className='radio-constainer'>
       <label>{label}</label>
-      {options_arr.map((item, index) => (
+      {options.map((item, index) => (
         <div key={index}>
           <input
             type='radio'
-            value={item}
+            value={item.value}
             onChange={handleChange}
             name={name}
           />
-          <label>{item}</label>
+          <label>{item.label}</label>
         </div>
       ))}
     </div>
